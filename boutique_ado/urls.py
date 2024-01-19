c624ab806dfe5bd5e5451ed25a6f26271e63369a
Amy Sawyer <amyleighsawyer97@gmail.com>
2024-01-19 19:45:32 +0000

    Added home app and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,7 +1,7 @@
 """boutique_ado URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import success_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('success/', success_view, name='success'),
-     path('', success_view, name='home'),
+    path('', include('home.urls')),
 ]
